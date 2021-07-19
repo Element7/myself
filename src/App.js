85b46f16024c13587aa85a2b1aa7cc3833777ab0
kacper <kjabcon@gmail.com>
2021-07-19 20:29:00 +0200

    feat: routes config

--- src/App.js
+++ src/App.js
@@ -1,5 +1,41 @@
+import React from "react";
+import ROUTES, { RenderRoutes } from "./utils/Routes";
+import { Link } from "react-router-dom";
+
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      {displayRouteMenu(ROUTES)}
+      <RenderRoutes routes={ROUTES} />
+    </div>
+  );
 }
 
 export default App;
+
+function displayRouteMenu(routes) {
+  function singleRoute(route) {
+    return (
+      <li key={route.key}>
+        <Link to={route.path}>
+          {route.key} ({route.path})
+        </Link>
+      </li>
+    );
+  }
+  return (
+    <ul>
+      {routes.map((route) => {
+        if (route.routes) {
+          return (
+            <React.Fragment key={route.key}>
+              {singleRoute(route)}
+              {displayRouteMenu(route.routes)}
+            </React.Fragment>
+          );
+        }
+        return singleRoute(route);
+      })}
+    </ul>
+  );
+}
--- src/components/atoms/button/Button.tsx
+++ src/components/atoms/button/Button.tsx
@@ -1,5 +1,12 @@
-export default function Button({onClick, text}) {
+import { FC } from "react"
+interface Props {
+  text: string;
+  onClick: () => void;
+}
+ 
+ const Button: FC<Props> = ({text, onClick}) => {
   return (
-   <button>{text}</button>
+   <button onClick={onClick}>{text}</button>
   )
 }
+export default Button
