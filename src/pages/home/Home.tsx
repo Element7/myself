2119d549ae27bb8b22a13b8a4593ea39674ae817
kacper <kjabcon@gmail.com>
2021-08-12 13:13:53 +0200

    backup

--- src/pages/home/Home.tsx
+++ src/pages/home/Home.tsx
@@ -1,24 +1,25 @@
 /* eslint-disable @typescript-eslint/prefer-as-const */
 import { CSSProperties, useContext, useLayoutEffect, useRef } from "react";
 
-import { createStyles, makeStyles, Theme } from "@material-ui/core/styles";
+import { createStyles, makeStyles } from "@material-ui/core/styles";
 import Typography from "@material-ui/core/Typography";
 import clsx from "clsx";
 import { gsap } from "gsap";
 
-import { ReactComponent as HomeCinema } from "../../assets/cinema-dark.svg";
+import { ReactComponent as HomeCinema } from "../../assets/cinema.svg";
 import { ReactComponent as Stars } from "../../assets/stars.svg";
-import Img from "../../components/atoms/img/Img";
 import { ThemeContext } from "../../providers/ThemeProvider";
 import {
+  lightLamp,
   onMainPictureClick,
   onMainPictureEnter,
   onMainPictureLeave,
+  switchOffLight,
 } from "../../utils/utils";
 
 const title = "Movie";
 const title2 = "Picker";
-const useStyles = makeStyles((theme: Theme) =>
+const useStyles = makeStyles(() =>
   createStyles({
     root: { flexGrow: 1 },
     title: {
@@ -32,7 +33,8 @@ const useStyles = makeStyles((theme: Theme) =>
 const starsStyle: CSSProperties = {
   position: "fixed",
   top: "64px",
-  left: "0",
+  left: "50%",
+  transform: "translateX(-50%)",
   zIndex: 2,
 };
 const imgStyle: CSSProperties = {
@@ -46,32 +48,26 @@ const imgStyle: CSSProperties = {
   zIndex: 10,
 };
 const Home: React.FC = () => {
-  const { theme } = useContext(ThemeContext);
-  const titleRef = useRef<HTMLDivElement>(null);
+  const { theme, dispatch } = useContext(ThemeContext);
+  const wrapperRef = useRef<HTMLDivElement>(null);
   const imgRef = useRef<HTMLDivElement>(null);
   const switchTl = gsap.timeline({
-    defaults: { pause: true, ease: "power3.inOut", yoyo: true },
+    defaults: { ease: "power3.inOut", yoyo: true },
   });
   const classes = useStyles();
 
   useLayoutEffect(() => {
     let elements;
     let elementsArr;
-    if (titleRef.current !== null) {
-      elements = titleRef.current.children;
+    if (wrapperRef.current !== null) {
+      elements = wrapperRef.current.children;
       elementsArr = Array.from(elements);
-      console.log(elements);
-
       const mainTitle = elementsArr[0];
-      console.log(mainTitle);
-
       const mainTitleChars = mainTitle.childNodes;
 
       const tl = gsap.timeline({
         defaults: { ease: "power3.inOut" },
       });
-      console.log(mainTitleChars);
-
       gsap.set([mainTitleChars], { autoAlpha: 0 });
       mainTitleChars.forEach((e) =>
         tl.fromTo(
@@ -84,9 +80,30 @@ const Home: React.FC = () => {
     }
   }, []);
 
+  useLayoutEffect(() => {
+    if (wrapperRef.current !== null) {
+      const elements = wrapperRef.current.children;
+      const starsImg = elements[1];
+      gsap.set(starsImg, { autoAlpha: 0, transformOrigin: "60% 50%" });
+      const tl = gsap.timeline({
+        defaults: { ease: "power3.inOut" },
+      });
+      if (theme === "light") {
+        switchOffLight(tl, imgRef);
+        tl.to(starsImg, { autoAlpha: 0 });
+      } else {
+        lightLamp(tl, imgRef);
+        tl.fromTo(
+          starsImg,
+          { scale: 0 },
+          { duration: 1, autoAlpha: "1", scale: 1 }
+        );
+      }
+    }
+  }, [theme]);
+
   return (
-    <div ref={titleRef}>
-      {theme === "dark" && <Stars style={starsStyle} />}
+    <div ref={wrapperRef}>
       <Typography className={classes.title} variant="h2">
         {Array.from(title).map((e, i) => (
           // eslint-disable-next-line react/no-array-index-key
@@ -99,9 +116,10 @@ const Home: React.FC = () => {
           <div key={i}>{e}</div>
         ))}
       </Typography>
+      <Stars style={starsStyle} />
       <div
-        onClick={() => onMainPictureClick(imgRef, switchTl)}
-        onKeyPress={() => onMainPictureClick(imgRef, switchTl)}
+        onClick={() => onMainPictureClick(imgRef, switchTl, theme, dispatch)}
+        onKeyPress={() => onMainPictureClick(imgRef, switchTl, theme, dispatch)}
         onMouseEnter={() => onMainPictureEnter(imgRef, switchTl)}
         onMouseLeave={() => onMainPictureLeave(imgRef, switchTl)}
         ref={imgRef}
--- src/utils/utils.js
+++ src/utils/utils.js
@@ -1,12 +1,10 @@
 /* eslint-disable @typescript-eslint/explicit-module-boundary-types */
-import { gsap } from "gsap";
 
 export const onMainPictureEnter = (ref, tl) => {
   if (ref.current !== null) {
     const [elements] = ref.current.children;
     const switchElement = elements.getElementById("switch");
-    // alert("aa");
-    tl.fromTo(switchElement, { y: "0" }, { duration: 1, y: "+=5" }).to(
+    tl.fromTo(switchElement, { y: "0" }, { duration: 0.2, y: "+=5" }).to(
       switchElement,
       {
         y: "0",
@@ -21,11 +19,32 @@ export const onMainPictureLeave = (ref, tl) => {
     tl.to(switchElement, { duration: 0, y: "+=0" });
   }
 };
-export const onMainPictureClick = (ref, tl) => {
+export const onMainPictureClick = (ref, tl, theme, dispatch) => {
   if (ref.current !== null) {
     const [elements] = ref.current.children;
-    const switchElement = elements.getElementById("lamp");
+    const lightElement = elements.getElementById("light");
+    if (theme === "light") {
+      dispatch({ type: "SET_DARK_MODE" });
+      tl.to(lightElement, { duration: 1, fill: "#F9A825", autoAlpha: "1" });
+    } else {
+      dispatch({ type: "SET_LIGHT_MODE" });
+      tl.to(lightElement, { duration: 1, fill: "#EEE", autoAlpha: "0.67" });
+    }
+  }
+};
 
-    tl.fromTo(switchElement, { fill: "#000" }, { duration: 1, fill: "#fff" });
+export const lightLamp = (tl, ref) => {
+  if (ref.current !== null) {
+    const [elements] = ref.current.children;
+    const lightElement = elements.getElementById("light");
+    tl.to(lightElement, { duration: 1, fill: "#F9A825", autoAlpha: "1" });
+  }
+};
+
+export const switchOffLight = (tl, ref) => {
+  if (ref.current !== null) {
+    const [elements] = ref.current.children;
+    const lightElement = elements.getElementById("light");
+    tl.to(lightElement, { duration: 1, fill: "#EEE", autoAlpha: "0.67" });
   }
 };
